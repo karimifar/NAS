808fa138a2cd1fb329d372af6bd062441656bb22
Karimifar <em.karimifar@gmail.com>
2019-09-24 17:07:42 -0500

    closing day

--- assets/js/app.js
+++ assets/js/app.js
@@ -39,6 +39,9 @@ function queryZip(zip){
     
 }
 
+
+
+//breakdown lines
 var lineGenerator = d3.line()
 	.curve(d3.curveBasis);
 
@@ -48,8 +51,8 @@ var points = [
 	[150, 90],
 ];
 var points2 = [
-    [78, 0],
-    [78,180],
+    [80, 0],
+    [80,180],
 	[150, 180],
 ];
 
@@ -66,3 +69,51 @@ d3.select('svg.break-down')
     d3.select('path.second')
     .attr('d', pathData2)
     .attr("class","curve second")
+
+
+function createBarChart(){
+    var margin = {top: 50, right: 50, bottom: 50, left: 50};
+    var width = 300;
+    var height= 200;
+    var n = 5;
+    var xScale = d3.scaleLinear()
+        .domain(["2013", "2014", "2015", "2016", "2017"])
+        .range([height,0]);
+
+    // 6. Y scale will use the randomly generate number 
+    var yScale = d3.scaleLinear()
+        .domain([0, 2]) // input 
+        .range([height, 0]); // output 
+
+    var line = d3.line()
+        .x(function(d,i){return xScale(i);})
+        .y(function(d){return yScale(d.y);})
+        .curve(d3.curveMonotoneX)
+
+    //replace with actual data
+    var dataset = d3.range(n).map(function(d){return{"y":d3.randomUniform(1)()}})
+    console.log(dataset)
+    var svg= d3.select(".lineChart")
+        .attr("width", width+margin.left+margin.right)
+        .attr("height", height+margin.top+margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," +margin.top+")");
+
+        svg.append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(0," + height + ")")
+            .call(
+                d3.axisBottom(xScale)
+                // .tickFormat(d3.format(",.2s"))
+                ); // Create an axis component with d3.axisBottom
+            
+
+        svg.append("g")
+            .attr("class", "y axis")
+            .call(d3.axisLeft(yScale)); // Create an axis component with d3.axisLeft
+        
+        svg.append("path")
+            .datum(dataset) // 10. Binds data to the line 
+            .attr("class", "line") // Assign a class for styling 
+            .attr("d", line); // 11. Calls the line generator 
+}
